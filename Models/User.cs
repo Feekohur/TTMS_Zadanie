0ba241fb0f0146a532ba2362eddccd57cc5de20d
Feekohur <dudadudadudadudada@gmail.com>
2022-05-17 12:16:52 +0200

    Dodaj pliki projektów.

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace TTMS_Zadanie.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Hasło { get; set; }
+        public string Email { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TTMS_Zadanie.Models;
+
+namespace TTMS_Zadanie.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<User> workers = new List<User>();
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            workers.Add(new User());
+            workers.Add(new User());
+            workers.Add(new User());
+            workers.Add(new User());
+            workers.Add(new User());
+        }
+    }
+}
